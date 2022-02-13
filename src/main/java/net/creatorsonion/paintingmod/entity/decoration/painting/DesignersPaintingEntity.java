8c17129a963f6dbee9fca5d6165b5915e39da7a9
andantet <orrdaniel73@gmail.com>
2022-02-13 10:14:25 +0000

    remove unnecessary source code heading

--- src/main/java/net/creatorsonion/paintingmod/entity/decoration/painting/DesignersPaintingEntity.java
+++ src/main/java/net/creatorsonion/paintingmod/entity/decoration/painting/DesignersPaintingEntity.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package net.creatorsonion.paintingmod.entity.decoration.painting;
 
 import net.creatorsonion.paintingmod.entity.PMEntityRegistry;
